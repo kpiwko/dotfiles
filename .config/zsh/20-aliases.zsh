be251055f4ba434b8e55898e3a1e60a51d2106b7
Karel Piwko <kpiwko@redhat.com>
2025-08-27 18:32:12 +0200

    Minor updates for `maskenv` command - sorting

--- .config/zsh/20-aliases.zsh
+++ .config/zsh/20-aliases.zsh
@@ -1,4 +1,4 @@
 alias ll='ls -la -h --color'
 alias pass="gopass"
 alias config='/usr/bin/git --git-dir=/Users/kpiwko/.dotfiles/ --work-tree=/Users/kpiwko'
-alias maskenv='env | sed -E "/(KEY|TOKEN|PASSWORD)/I s/^([^=]+)=([^=]{0,5}).*/\1=\2.../"'
+alias maskenv='env | sort | sed -E "/(KEY|TOKEN|PASSWORD)/I s/^([^=]+)=([^=]{0,5}).*/\1=\2.../"'
